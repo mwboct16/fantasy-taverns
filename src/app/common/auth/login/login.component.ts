36e0a2cfbc3cecc36732b03092755436d0135e1b
mwboct16@gmail.com <mwboct16@gmail.com>
2020-03-04 21:41:35 -0500

    HW 1: added signup mode and button funcitons

--- src/app/common/auth/login/login.component.ts
+++ src/app/common/auth/login/login.component.ts
@@ -1,15 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
 @Component({
     templateUrl: './login.component.html',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
     userName = '';
     password = '';
+    ShowSignedUp = false;
 
-    constructor(private router: Router, private authService: AuthService) {}
+    constructor(private router: Router, private authService: AuthService) { }
+
+    ngOnInit(): void {
+
+        console.log({ ...this })
+    }
+
+    ngOnDestroy(): void {
+        console.log('is destroyed')
+    }
+
+    SignUp(): void {
+        const payload = {
+            userName: this.userName,
+            password: this.password,
+        };
+
+        console.log(payload)
+    }
+    
+
+    toggleSignUp(): void {
+        this.userName = '',
+        this.password = '',
+
+        this.ShowSignedUp = !this.ShowSignedUp;
+    }
 
     login(): void {
         this.authService.login(this.userName, this.password).subscribe(
